cc5e5f9cd983c8b1aa9d73cd9f9950d32a8fcdca
Thiago Rodrigues de Souza <thiago.engh@gmail.com>
2018-11-15 23:11:44 -0300

    Adicioandno componentes: Cadastro e Gerência de pedidos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css'
+
+import Topo from './Topo'
+import CadastroForm from './CadastroForm'
+import Rodape from './Rodape'
+import StatusForm from './StatusForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Topo/>
+        <StatusForm/>
+        <Rodape/>
       </div>
     );
   }
